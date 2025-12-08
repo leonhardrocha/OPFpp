4d51de67a666b217966c525ba281c3ab4a20d128
Leonardo Rocha <113632710+leonhardrocha@users.noreply.github.com>
2025-12-08 18:57:31 -0300

    msvc build working

---
+++ CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: D:/src/projects/helloworld
+# Build directory: D:/src/projects/helloworld
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/googletest-build")
+subdirs("_deps/pybind11-build")
+subdirs("src")
+subdirs("apps")
+subdirs("python")
+subdirs("tests")
