6f593e17accb990b64577b2186fe7f7687eb60c5
Leonardo Rocha <113632710+leonhardrocha@users.noreply.github.com>
2026-01-13 17:36:20 -0300

    update.; now pybind11 bindings are fixed (added Python to PATH)

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,13 +1,24 @@
-#include "helloworld.h"
 #include <vector>
 #include <string>
 #include <sstream>
+#include <iostream>
+#include <span>
+#include <array>
 
 std::string get_hello_message() {
-    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    std::array<std::string, 6> msg_array {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    std::span<std::string> msg(msg_array);
+    
     std::stringstream result;
     for (const std::string& word : msg) {
         result << word << " ";
     }
     return result.str();
+}
+
+
+int main() {
+    std::string message = get_hello_message();
+    std::cout << message << std::endl;
+    return 0;
 }
